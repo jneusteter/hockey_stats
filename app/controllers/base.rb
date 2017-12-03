424031fa747ca820669adff67ba3af6629cfeb4c
John Neusteter <jneusteter@gmail.com>
2017-12-03 10:45:59 -0500

    Working home page.

--- app/controllers/base.rb
+++ app/controllers/base.rb
@@ -1,26 +1,5 @@
 HockeyStatsTracker::App.controllers :base do
-  
-  # get :index, :map => '/foo/bar' do
-  #   session[:foo] = 'bar'
-  #   render 'index'
-  # end
-
-  # get :sample, :map => '/sample/url', :provides => [:any, :js] do
-  #   case content_type
-  #     when :js then ...
-  #     else ...
-  # end
-
-  # get :foo, :with => :id do
-  #   "Maps to url '/foo/#{params[:id]}'"
-  # end
-
-  # get '/example' do
-  #   'Hello world!'
-  # end
-  
-  get :index do
-
+  get :index, map: '/' do
+    render 'index'
   end
-
 end
