8947876d3ce79b8ebf45788e0aaa6577d367c938
John Neusteter <jneusteter@gmail.com>
2017-12-03 11:15:49 -0500

    Added players controller

---
+++ app/helpers/players_helper.rb
@@ -0,0 +1,13 @@
+# Helper methods defined here can be accessed in any controller or view in the application
+
+module HockeyStatsTracker
+  class App
+    module PlayersHelper
+      # def simple_helper_method
+      # ...
+      # end
+    end
+
+    helpers PlayersHelper
+  end
+end
