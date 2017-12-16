f06d8db6a24871003534fc50aaaf0e02a2f61216
John Neusteter <jneusteter@gmail.com>
2017-12-16 15:32:33 -0500

    Added two player helpers

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -6,6 +6,14 @@ module HockeyStatsTracker
       def total_plus_minus(player_id)
         PlusMinus.total_plus_minus(player_id)
       end
+
+      def total_assist_one(player_id)
+        Goal.where(assist_one: player_id).count
+      end
+
+      def total_assist_two(player_id)
+        Goal.where(assist_two: player_id).count
+      end
     end
 
     helpers PlayersHelper
