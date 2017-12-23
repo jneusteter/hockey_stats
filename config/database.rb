19743d59d235cb9146de7d51c57081b59f58e42d
John Neusteter <jneusteter@gmail.com>
2017-12-23 08:41:50 -0500

    Added json serializer to sequel

--- config/database.rb
+++ config/database.rb
@@ -1,5 +1,6 @@
 # Sequel::Model.plugin(:schema)
 Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
+Sequel::Model.plugin :json_serializer
 Sequel::Model.db = case Padrino.env
   when :development then Sequel.connect("sqlite://db/hockey_stats_tracker_development.db", :loggers => [logger])
   when :production  then Sequel.connect("sqlite://db/hockey_stats_tracker_production.db",  :loggers => [logger])
