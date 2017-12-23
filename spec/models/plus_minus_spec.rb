977dc72e02cb89f1b037e46778c8cb4178754ebf
John Neusteter <jneusteter@gmail.com>
2017-12-23 13:42:54 -0500

    Added test for total_by_player method

--- spec/models/plus_minus_spec.rb
+++ spec/models/plus_minus_spec.rb
@@ -1,4 +1,14 @@
 require 'spec_helper'
 
 RSpec.describe PlusMinus do
+  describe "#total_by_player" do
+    it "returns 10 when total sum is +10" do
+      FactoryBot.create_list(:plus_minus, 10)
+      expect(PlusMinus.total_by_player(10)).to eq(10)
+    end
+    it "returns -10 when total sum is -10" do
+      FactoryBot.create_list(:plus_minus, 10, plus_minus: -1)
+      expect(PlusMinus.total_by_player(10)).to eq(-10)
+    end
+  end
 end
