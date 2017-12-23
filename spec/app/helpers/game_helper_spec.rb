553b4a686d5a6a924ee53a3af53633dd238f5226
John Neusteter <jneusteter@gmail.com>
2017-12-23 08:41:19 -0500

    First test

--- spec/app/helpers/game_helper_spec.rb
+++ spec/app/helpers/game_helper_spec.rb
@@ -1,4 +1,13 @@
 require 'spec_helper'
 
 RSpec.describe "HockeyStatsTracker::App::GameHelper" do
+  describe '#format_date' do
+    let(:helpers){ Class.new }
+    before { helpers.extend HockeyStatsTracker::App::GameHelper }
+    subject { helpers }
+
+    it "return clean date" do
+      expect(format_date("2017-10-06 19:15:00 -0400")).to eq("Oct 10 2017 4:15pm")
+    end
+  end
 end
