d92968f9c6d810e9dd79506dbbdd34471221dd28
John Neusteter <jneusteter@gmail.com>
2017-12-23 08:40:34 -0500

    Removed boilerplate tests

--- spec/app/controllers/players_controller_spec.rb
+++ spec/app/controllers/players_controller_spec.rb
@@ -1,13 +1,4 @@
 require 'spec_helper'
 
 RSpec.describe "/players" do
-  pending "add some examples to #{__FILE__}" do
-    before do
-      get "/players"
-    end
-
-    it "returns hello world" do
-      expect(last_response.body).to eq "Hello World"
-    end
-  end
 end
