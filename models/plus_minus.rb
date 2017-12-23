26a1eb235c012fc2b146e7315b629842314159cc
John Neusteter <jneusteter@gmail.com>
2017-12-23 13:42:15 -0500

    Added the total_by_player method

--- models/plus_minus.rb
+++ models/plus_minus.rb
@@ -2,7 +2,7 @@ class PlusMinus < Sequel::Model
   many_to_one :player
   many_to_one :game
 
-  def self.total_plus_minus(player_id)
-    self.where(player_id: player_id).sum(:plus_minus)
+  def self.total_by_player(player_id)
+    where(player_id: player_id).sum(:plus_minus)
   end
 end
