d31e87ca0fd9eab4dc8e07f6d41b527fd4679e7a
John Neusteter <jneusteter@gmail.com>
2018-10-07 13:14:07 -0400

    Fixed base controller

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Start Production Server
 ``` sh
-  padrino start -e production -h 0.0.0.0 -p 2018
+  padrino start -e production -h 0.0.0.0 -p 2018 -d
 ```
 
--- app/controllers/base.rb
+++ app/controllers/base.rb
@@ -1,5 +1,7 @@
 HockeyStatsTracker::App.controllers :base do
   get :index, map: '/' do
+    @title = 'Players'
+    @players = Player.all
     render 'index'
   end
 end
